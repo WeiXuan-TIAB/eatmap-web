3b8efdab2c24e7d1ce425b2c04790fc9bb66586c
TIAB <wbigbear82230@gmail.com>
2025-12-09 22:40:29 +0800

    chore: update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [RouterOutlet, RouterModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('eatmap-web');
